03e613262402cf9760cd73929ce96b9de8d97088
Haochen Wang <wanghaochen@uchicago.edu>
2023-04-10 17:44:19 -0500

    udpate colmap data extraction tool

--- scripts/extract_from_colmap.py
+++ scripts/extract_from_colmap.py
@@ -1,23 +1,45 @@
-from pathlib import Path
-import numpy as np
+"""
+This script is used to extract info from colmap database. It uses
+pycolmap, https://github.com/colmap/pycolmap, a library developed by students in
+Pollefeys' group.
 
+The library as of now (2023.4.10) doesn't have documentation. You can guess its
+behavior from the names of python bindings, such as this
+https://github.com/colmap/pycolmap/blob/401f82658cdad1e8b657c77381863f9e261c7c3c/reconstruction/camera.cc#L107
+"""
+
+from pathlib import Path
+import pickle
+import numpy as np
+import pycolmap
 
 # # for mac os; somehow colmap import triggers error when using pudb
 # import os
 # os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+import click
 
-def main():
-    import pycolmap
-    import pickle
 
-    root = Path("~/work/colmap_data/0").expanduser()
+@click.command()
+@click.option(
+    '-r', '--root', default="/scratch/nerf_data/360_v2/garden/sparse/0",
+    help='dir where the colmap database .bin files are stored'
+)
+@click.option(
+    '-o', '--ofname', default="recons.pkl", help='output pickle file name'
+)
+def main(root, ofname):
+    root = Path(root)
+
     reconstruction = pycolmap.Reconstruction(root)
     print(reconstruction.summary())
+
+    # reconstruction.export_CAM("./")  alternatively consider this for camera exporting
 
     poses = []
     Ks = []
     fnames = []
+    HWs = []
     for image_id, image in reconstruction.images.items():
         fnames.append(image.name)
 
@@ -26,11 +48,14 @@ def main():
         poses.append(po)
 
         cam = reconstruction.cameras[image.camera_id]
+        HWs.append([cam.height, cam.width])
+        # WARN: this is disregarding the camera model, pinhole/fisheye, etc.
         intr = cam.calibration_matrix()
         Ks.append(intr)
 
     poses = np.array(poses)
     Ks = np.array(Ks)
+    HWs = np.array(HWs)
 
     pts_xyz = []
     pts_rgb = []
@@ -48,11 +73,13 @@ def main():
         "fnames": fnames,
         "poses": poses,
         "Ks": Ks,
+        "HWs": HWs,
         "pts_xyz": pts_xyz,
         "pts_rgb": pts_rgb
-    }
+    }  # could have dumped it as .json
 
-    with open("recons.pkl", 'wb') as f:
+    ofname = Path(ofname).with_suffix(".pkl")
+    with open(ofname, 'wb') as f:
         pickle.dump(recons, f, pickle.HIGHEST_PROTOCOL)
 
 
