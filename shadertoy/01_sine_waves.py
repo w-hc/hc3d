e858e4d7294cf1d14cc711782bb4cc483c48f586
Haochen Wang <wanghaochen@uchicago.edu>
2024-03-21 15:32:17 -0500

    add

--- shadertoy/01_sine_waves.py
+++ shadertoy/01_sine_waves.py
@@ -2,7 +2,7 @@
 this is a pytorch imitation of the shadertoy programming environment for procedural graphics / image generation
 """
 import torch
-from utils import entry, device, two_pi, make_tsr, cos, coords_take
+from utils import entry, device, two_pi, make_tsr, cos, coords_take, mix, smooth_step
 
 
 def dummy_shader(xy_coords, canvas_wh, t):
@@ -25,7 +25,7 @@ def horizontal_sine_wave(xy_coords, canvas_wh, t):
     xs, ys = xy_coords.T
     rgbs = torch.zeros(len(xy_coords), 3, device=device)
 
-    # horizontal period 0.7 (debugs that x grows from left to right); as t goes, shift right by 3 periods
+    # horizontal period 0.7 (debugs that x grows from left to right); as t goes, shift [right to left] by 3 periods
     tick = xs / 0.7 + t * 3
     rgbs[:, 0] = 0.5 + 0.5 * cos(tick * two_pi)
     return rgbs
@@ -38,15 +38,48 @@ def artsy_sine(xy_coords, canvas_wh, t):
     return rgbs
 
 
+"""
+vec3 rulerColor(float t) {
+    t = clamp(log(t+1.0), 0.0, 1.0);
+    return mix(
+        mix(vec3(0., .1., 1.), vec3(1., .1, 0.), t*5.), 
+        vec3(1.0), 
+        smoothstep(.2, .5, t)
+    );
+}
+"""
+def special(xy_coords, canvas_wh, t):
+    xy_coords = xy_coords / canvas_wh
+    xs, ys = xy_coords.T
+    ts = xs
+
+    # ts expected to be positive
+    ts = torch.clamp(torch.log(1.0 + ts), 0.0, 1.0)
+    # ts = torch.clamp(ts, 0.0, 1.0)
+
+    def _m(*args):
+        return make_tsr(args).unsqueeze(-1)
+
+    colors = mix(
+        mix(_m(0.0, 0.1, 1.0), _m(1.0, 0.1, 0.0), ts * 20.),
+        _m(1.0, 1.0, 1.0),
+        smooth_step(0.2, 0.5, ts)
+    )
+    return colors.T
+
+
 def main():
-    canvas_wh = (256, 128)
+    # canvas_wh = (256, 128)
 
-    entry(vertical_sine_wave, canvas_wh, fname="out/01_vertical_sine")
+    # entry(vertical_sine_wave, canvas_wh, fname="out/01_vertical_sine")
 
-    entry(horizontal_sine_wave, canvas_wh, num_frames=90, fps=30, fname="out/01_horizontal_sine")
+    # entry(horizontal_sine_wave, canvas_wh, num_frames=90, fps=30, fname="out/01_horizontal_sine")
 
-    canvas_wh = (512, 256)
-    entry(artsy_sine, canvas_wh, num_frames=90, fps=30, fname="out/01_artsy")
+    # canvas_wh = (512, 256)
+    # entry(artsy_sine, canvas_wh, num_frames=90, fps=30, fname="out/01_artsy")
+
+    canvas_wh = (256, 256)
+    entry(special, canvas_wh, fname="out/01_special_color")
 
 
 if __name__ == '__main__':
